c7bd049413cce4f7557644afeaf5600a13867b1b
kumakki <flandrescarlet0830495@yahoo.co.jp>
2026-01-19 14:00:31 +0900

    オリジナルボタン作成

---
+++ components/BaseLinkButton.vue
@@ -0,0 +1,22 @@
+<template>
+    <button>
+        <slot />
+    </button>
+</template>
+
+<style scoped lang="scss">
+button{
+    padding: 10px 20px;
+    background-color: transparent;
+    color: black;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 16px;
+    transition: background-color 0.3s ease;
+
+    &:hover {
+        background-color: lightgray;
+    }
+}
+</style>
---
+++ pages/test.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <BaseLinkButton>テストボタン</BaseLinkButton>
+    </div>
+</template>
