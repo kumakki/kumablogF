c3b33fb6d620295be8dea59e5e4b88f8c1914d14
kumakki <flandrescarlet0830495@yahoo.co.jp>
2026-01-21 08:24:31 +0900

    ヘッダーにボタン表示

---
+++ components/header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="to-right">
+        <BaseLinkButton>My Accounts</BaseLinkButton>
+    </div>
+</template>
+
+<style scoped Lang="scss">
+.to-right {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    padding-top: 4px;
+    padding-bottom: 10px;
+    border-bottom: 1px solid gray;
+}
+</style>
