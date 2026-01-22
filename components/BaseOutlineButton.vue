6f63358c4c9968f02414f2be34d9f9e6a64c6006
kumakki <flandrescarlet0830495@yahoo.co.jp>
2026-01-23 08:55:59 +0900

    ヘッダーを調整

---
+++ components/BaseOutlineButton.vue
@@ -0,0 +1,22 @@
+<template>
+    <button>
+        <slot />
+    </button>
+</template>
+
+<style scoped lang="scss">
+button{
+    padding: 10px 20px;
+    background-color: transparent;
+    color: black;
+    border: solid 1px black;
+    border-radius: 20px;
+    cursor: pointer;
+    font-size: 16px;
+    transition: background-color 0.3s ease;
+
+    &:hover {
+        background-color: lightgray;
+    }
+}
+</style>
--- components/header.vue
+++ components/header.vue
@@ -1,16 +1,27 @@
 <template>
-    <div class="to-right">
-        <BaseLinkButton>My Accounts</BaseLinkButton>
+    <div class="header">
+        <div class="left">
+            <BaseLinkButton>My Blog</BaseLinkButton>
+        </div>
+        <div class="right">
+            <BaseOutlineButton>My Accounts</BaseOutlineButton>
+        </div>
     </div>
 </template>
 
 <style scoped Lang="scss">
-.to-right {
+.header {
     display: flex;
-    justify-content: flex-end;
+    justify-content: space-between;
+    /* 左右に分ける */
     align-items: center;
-    padding-top: 4px;
-    padding-bottom: 10px;
+    /* 縦方向を中央揃え */
+    padding: 0 20px;
+    height: 60px;
     border-bottom: 1px solid gray;
+}
+
+.right {
+    display: flex;
 }
 </style>
