3b724d9b5760c55a229b9f6493a03de949e418fe
kumakki <flandrescarlet0830495@yahoo.co.jp>
2026-01-23 16:00:12 +0900

    テキストボックス追加

---
+++ components/BaseInputBox.vue
@@ -0,0 +1,14 @@
+<template>
+    <input class="input"/>
+</template>
+
+<style scoped lang="scss">
+    .input {
+        width: 100%;
+        padding: 10px;
+        margin: 4px;
+        border: 1px solid #ccc;
+        border-radius: 10px;
+        font-size: 16px;
+    }
+</style>
